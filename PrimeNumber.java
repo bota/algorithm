57ee269c1aed41d113b8b68b493043cfd57d0c81
bota <kkunihiro424@gmail.com>
2013-11-17 11:26:02 +0900

    second commit

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,4 +1,4 @@
-package programing.java;
+package programming.java;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,20 +10,18 @@ public class PrimeNumber {
 		outLog(num);
 	}
 	/**
-	 * 
+	 * output the number of prime number
+	 * ex. 10 -> 4
 	 * @param num
 	 */
 	public static void outLog(int num) {
 		int[] array = new int[num];
 		List<Integer> list = new ArrayList<Integer>();
 		for (int i = 2; i < num; i++) {array[i] = i;};
-		//‘f”‚Å‚È‚¢”‚ğ0‚É•ÏX‚·‚é
+		// change the number into 0 except for a prime number
 		for (int i = 2; i < num; i++) {
 			for (int j = i + i; j < num; j+=i) {
 				array[j] = 0;
-			}
-			if (array[i] != 0) {
-				System.out.println(array[i]);
 			}
 		}
 		for (int i = 0; i < num; i++) {if (array[i] != 0) {list.add(array[i]);}}
